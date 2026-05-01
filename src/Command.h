2318eb3e7932326321bf2f7b4003aa18f0f09c01
noamcalf <calf1708@gmail.com>
2026-05-01 20:27:46 +0300

    WOLT-28: Defined command struct and parser interface

---
+++ src/Command.h
@@ -0,0 +1,21 @@
+// In the compilation stage- if you see this file during this compilation, ignore it
+#pragma once
+
+#include <string>
+#include <vector>
+
+// An easy way to defer the different comman types
+enum class CommandType {
+    RECOMMEND,
+    ADD_PRODUCT,
+    EXIT,
+    INVALID
+};
+
+// The way the app should get the data from the user
+struct Command {
+    CommandType type;
+    std::vector<std::string> arguments;
+    // rawInput field is for returnning what the user enterd in case of an error
+    std::string rawInput;
+};
