b28ac23831b773b1db3b1a8caa4132b2c4cba30b
noamcalf <calf1708@gmail.com>
2026-06-14 12:47:28 +0300

    WOLT-181: Configure (CORS) Client Environment

---
+++ react-frontend/config/api.js
@@ -0,0 +1,10 @@
+// Set the basic URL path of the backend server for easy communication
+export const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+
+// Set shortcuts for each route of the API:
+export const API_ROUTES = {
+    LOGIN: `${BASE_URL}/api/tokens`,
+    REGISTER: `${BASE_URL}/api/users`,
+    RESTAURANTS: `${BASE_URL}/api/restaurants`,
+    SEARCH: `${BASE_URL}/api/search`
+};
